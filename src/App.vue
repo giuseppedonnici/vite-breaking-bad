11efe1c453aea6ac5073d3c18f3cdcafca44d653
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-17 14:42:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  components: {
+    AppHeader
+  }
+
+}
+</script>
+
+<template>
+  <AppHeader title="Yu-Gi-Oh Api"/>
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "AppHeader",
+    props: {
+        title: String
+    }
+}
+</script>
+
+<template>
+    <header>
+        <img class="m-4" src="../assets/img/logo.png" alt="">
+        <h1 class="container">{{ title }}</h1>
+    </header>
+</template>
+
+<style scoped lang="scss">
+    header {
+        line-height: 80px;
+        background-color: white;
+        color:black;
+        img {
+            width: 50px;
+        }
+        h1 {
+            display: inline-block;
+            vertical-align: middle;
+        }
+    }
+</style>
