ca857f6be0e8b0389c616655e5ba331b1ea6ee6d
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-18 15:24:05 +0200

    Add: AppSelect

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import CardList from "./components/CardList.vue";
+import AppSelect from "./components/AppSelect.vue";
 import axios from "axios";
 import {store} from "./store";
 
 export default {
   components: {
     AppHeader,
-    CardList
+    CardList,
+    AppSelect
   },
   data() {
     return {
@@ -16,12 +18,36 @@ export default {
   },
   mounted() {
     store.loading = true;
-    axios.get(store.apiURL).then((resp) => {
-      console.log(resp.data.data);
+    axios.get(this.store.apiURL, {
+      params: {
+        num: 20,
+        offset: 0
+      }
+    }).then((resp) => {
       this.store.cards = resp.data.data;
       store.loading = false;
-      console.log(store.cards);
     })
+  },
+  methods: {
+    handleFilter() {
+      console.log("funziona");
+      store.loading = true;
+      const params = {};
+      if(this.store.selectedArchetype) {
+        params.archetype = this.store.selectedArchetype;
+      }
+
+      axios.get(this.store.apiURL, {
+        params
+      }).then(resp => {
+        this.store.cards = resp.data.data;
+      }).catch(error => {
+        console.log(error);
+      }).finally(() => {
+        this.store.loading = false;
+      })
+
+    }     
   }
 
 }
@@ -29,7 +55,8 @@ export default {
 
 <template>
   <AppHeader title="Yu-Gi-Oh Api"/>
-  <main class="d-flex align-items-center p-5">
+  <main class="d-flex flex-column align-items-center p-5 g-4">
+    <AppSelect @filter="hadleFilter" />
     <CardList />
   </main>
 
---
+++ src/components/AppSelect.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: "AppSelect",
+    emits: ["filter"],
+    data() {
+        return {
+            store,
+            achetypes: ["Alien", "Ally of Justice", "Ancient Gear"]
+        }
+    }
+}
+</script>
+
+<template>
+    <h3>Archtype:</h3>
+    <select class="form-select w-25 mb-4" name="archetype" id="archetype" v-model="store.selectedArchetype" @change="$emit('filter')">
+        <option value="">All</option>
+        <option :value="archetype" v-for="archetype in achetypes">{{ archetype }}</option>
+    </select>
+</template>
